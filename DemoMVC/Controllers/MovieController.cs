67cf9a8aadb845b3cc5e6aab4ccfadfdfc315f32
Khun <lykhun@gmail.com>
2025-09-12 14:02:18 +0200

    Add project files.

---
+++ DemoMVC/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZGQ1MDYyNjQ5YTc2MTg1YjVhY2YzMjkxMTUyN2E4OCIsIm5iZiI6MTc1NTU4OTM3Ni4yNTksInN1YiI6IjY4YTQyYjAwYjVhYjFmNzRjNjU2N2UzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pTcfUtfJkDiMmNfOu4g7LT24Sof0kyXMpGajJR6CvF4");
+
+            MovieResult? model = client
+                .GetFromJsonAsync<MovieResult>("https://api.themoviedb.org/3/discover/movie").Result;
+
+            return View(model);
+        }
+    }
+}
