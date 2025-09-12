ad0161a773251e49d039ffbbbb59787f2b0e0e22
Khun <lykhun@gmail.com>
2025-09-12 16:23:52 +0200

    pagination

--- DemoMVC/Controllers/MovieController.cs
+++ DemoMVC/Controllers/MovieController.cs
@@ -5,13 +5,13 @@ namespace DemoMVC.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index([FromQuery]int page = 1)
         {
             using HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZGQ1MDYyNjQ5YTc2MTg1YjVhY2YzMjkxMTUyN2E4OCIsIm5iZiI6MTc1NTU4OTM3Ni4yNTksInN1YiI6IjY4YTQyYjAwYjVhYjFmNzRjNjU2N2UzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pTcfUtfJkDiMmNfOu4g7LT24Sof0kyXMpGajJR6CvF4");
 
             MovieResult? model = client
-                .GetFromJsonAsync<MovieResult>("https://api.themoviedb.org/3/discover/movie").Result;
+                .GetFromJsonAsync<MovieResult>("https://api.themoviedb.org/3/discover/movie?page=" + page).Result;
 
             return View(model);
         }
---
+++ DemoMVC/Models/Pagination.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoMVC.Models
+{
+    public class Pagination
+    {
+        public int CurrentPage { get; set; }
+        public string Controller { get; set; } = null!;
+        public string Action { get; set; } = null!;
+        public int PageMax { get; set; }
+        public int Range { get; set; }
+    }
+}
