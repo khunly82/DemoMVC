1db07e100e6cd2f114ebbbe04821f273a0dad5e1
Bvanoo <bvanoostveldt@gmail.com>
2025-09-19 14:02:09 +0200

    Envois mail

---
+++ DemoMVC/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(ContactFormModel model )
+        {
+
+            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("b2c30afa384761", "3b7808f937d0cc"),
+                EnableSsl = true
+            };
+            client.Send(model.Email, "wendie.mazy@gmail.com", "Message envoyer depuis un formulaire ***** **** ********", model.Msg);
+            System.Console.WriteLine("Sent");
+            return View("Index");
+        }
+    }
+}
---
+++ DemoMVC/Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoMVC.Models
+{
+    public class ContactFormModel
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Msg { get; set; }
+    }
+}
