054ccf1fb3414d17eddd6e99abc43931d3067209
Khun <lykhun@gmail.com>
2025-09-25 13:20:42 +0200

    index customer

---
+++ DemoMVC/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿
+using DemoMVC.Entities;
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DemoMVC.Controllers
+{
+    public class CustomerController(StockContext _db) : Controller
+    {
+        public IActionResult Index([FromQuery]string? search)
+        {
+            ViewBag.Search = search; 
+
+            IQueryable<Customer> query = _db.Customers
+                // dans le cas ou on utilise un mapper, il faut ajouter le jointure de manière explicite
+                .Include(c => c.Orders);
+
+            if(search != null)
+            {
+                query = query.Where(c => c.LastName.StartsWith(search) || c.FirstName.StartsWith(search));
+            }
+
+            var customers = query.Select(c => new CustomerIndexViewModel(c))
+                .ToList();
+
+            return View(customers);
+        }
+    }
+}
---
+++ DemoMVC/Models/CustomerIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DemoMVC.Entities;
+
+namespace DemoMVC.Models
+{
+    public class CustomerIndexViewModel(Customer customer)
+    {
+        public string Ref { get; set; } = customer.Reference;
+        public string LastName { get; set; } = customer.LastName;
+        public string FirstName { get; set; } = customer.FirstName;
+        public int NbOrders { get; set; } = customer.Orders.Count;
+    }
+}
