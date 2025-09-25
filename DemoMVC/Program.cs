0fb8a0701291dc5ae28263259bd454aa4648ea1e
Khun <lykhun@gmail.com>
2025-09-25 09:32:46 +0200

    remove EF

--- DemoMVC/Program.cs
+++ DemoMVC/Program.cs
@@ -65,50 +65,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Movie}/{action=Index}/{id:int?}");
 
-// app.Run();
+app.Run();
 
-//List<string> strings = ["Mohamed", "Khun", "Wendy"];
-
-//List<string> filteredStrings = strings
-//    .Where(s => s.Contains("n"))
-//    .Select(s => s.ToUpper())
-//    .ToList();
-
-//foreach (string s in strings)
-//{
-//    if(s.Contains("n"))
-//    {
-//        filteredStrings.Add(s);
-//    }
-//}
-
-// Console.WriteLine(string.Join(",", filteredStrings));
-
-var db = app.Services.CreateScope()
-    .ServiceProvider.GetService<StockContext>();
-
-var p = db.Products
-    .Where(p => p.LastBuyingPrice < 30)
-    .Select(p => new { 
-        // selecttionner et mettre un alias
-        Name = p.Name.ToUpper(), 
-        Prix = p.Price 
-    })
-    .ToList();
-
-foreach (var product in p)
-{
-    Console.WriteLine(product.Name);
-    Console.WriteLine(product.Prix);
-    Console.WriteLine("---------------------");
-}
-
-
-// 1. select lastName, firstName FROM customer
-// 2. selectionner la ref, nom, prenom du client
-// dont la réference est C005
-// 3. selectionner le nom, le prenom, l'email en majuscule
-// des clients dont le  le nom ou le prenom  commence par "a"
-// 4. selectionner le nom, le prenom, l'email
-// des 2 premiers clients triés
-// par ordre decroissant sur le nom dont le nom ou le prenom commence par "a"
